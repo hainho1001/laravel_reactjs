9e35ead546c82799de4697bfd6b54d7f6a240ca2
Hai Vo <hainho1001@gmail.com>
2021-12-02 09:46:59 +0700

    move code

--- backend/app/Exceptions/Handler.php
+++ backend/app/Exceptions/Handler.php
@@ -2,11 +2,20 @@
 
 namespace App\Exceptions;
 
+use App\Http\Controllers\Api\ApiResponse;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\UnauthorizedException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponse;
     /**
      * A list of the exception types that are not reported.
      *
@@ -36,6 +45,80 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function (Throwable $exception, $request) {
+            if($request->wantsJson()) { 
+            
+                if ($exception instanceof ModelNotFoundException) {
+
+                    return $this->sendFailedResponse(
+                        'Model not found.',
+                        $this->statusNotFound
+                    );
+                }
+        
+                else if ($exception instanceof NotFoundHttpException) {
+                    $url = request()->fullUrl();
+
+                    return $this->sendFailedResponse(
+                        "The requested URL [$url] was not found on this server.",
+                        $this->statusNotFound
+                    );
+                }
+        
+                else if ($exception instanceof MethodNotAllowedHttpException) {
+        
+                    return $this->sendFailedResponse(
+                        $exception->getMessage(),
+                        $this->statusMethodNotAllowed
+                    );
+                }
+
+                else if ($exception instanceof UnauthorizedException) {
+                    
+                    return $this->sendFailedResponse(
+                        $exception->getMessage(),
+                        $this->statusMethodNotAllowed
+                    );
+                }
+
+                else if ($exception instanceof AuthenticationException) {
+                    
+                    return $this->sendFailedResponse(
+                        $exception->getMessage(),
+                        $this->statusUnauthorized
+                    );
+                }
+
+                else if ($exception instanceof ValidationException) {
+                    $error = '';
+                    foreach ($exception->errors() as $key => $value) {
+                        $error = $value[0];
+                    }
+                    return $this->sendFailedResponse(
+                        $error,
+                        $this->statusUnprocessableEntity
+                    );
+                }
+
+                else if ($exception instanceof QueryException) {
+                    
+                    return $this->sendFailedResponse(
+                        $exception->getMessage(),
+                        $this->statusUnprocessableEntity
+                    );
+                }
+
+                else if (!empty($exception->getMessage())) {
+                   
+                    return $this->sendFailedResponse(
+                        $exception->getMessage(),
+                        $this->statusInternalServerError
+                    );
+                }
+    
+            }
         });
     }
 }
---
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\RegisterUserRequest;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(RegisterUserRequest $request)
+    {
+        
+    }
+}
